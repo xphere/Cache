68cc4c4ad93f4c16b3c169e8f5195c071dedae2a
Berny Cantos <xphere81@gmail.com>
2012-10-05 16:22:14 +0200

    Generic Cache class customizable with Storage and Strategy

---
+++ lib/BCC/Cache/Cache.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the BCC\Cache package
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BCC\Cache;
+
+class Cache
+{
+    private $storage;
+    private $strategy;
+
+    public function __construct(StorageInterface $storage, StrategyInterface $strategy = null)
+    {
+        $this->storage = $storage;
+        $this->strategy = $strategy;
+    }
+
+    public function get($key, $default = null)
+    {
+        $item = $this->getItem($key);
+        return $item->miss() ? $default : $item->get();
+    }
+
+    public function getOrSet($key, $value, $ttl = null)
+    {
+        $item = $this->getItem($key);
+        if ($item->miss()) {
+            $item->set($value, $ttl);
+            return $value;
+        }
+        return $item->get();
+    }
+
+    public function getOrLazySet($key, \Closure $callback, $ttl = null)
+    {
+        $item = $this->getItem($key);
+        if ($item->miss()) {
+            $value = $callback();
+            $item->set($value, $ttl);
+            return $value;
+        }
+        return $item->get();
+    }
+
+    public function set($key, $value, $ttl = null)
+    {
+        $this->getItem($key)->set($value, $ttl);
+        return $this;
+    }
+
+    public function remove($key)
+    {
+        $item = $this->getItem($key);
+        $item->miss() || $item->remove();
+        return $this;
+    }
+
+    /**
+     * @return ItemInterface
+     */
+    public function getItem($key)
+    {
+        if ($this->strategy) {
+            $key = $this->strategy->getKey($key);
+        }
+        return $this->storage->getItem($key);
+    }
+}
