450cf7d00c2dec9ce729205483e81041a132c250
Berny Cantos <berny.cantos@ofertix.com>
2013-05-23 09:44:50 +0200

    add ArrayAccessStorage

---
+++ src/Berny/Cache/Storage/ArrayAccess/Item.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Berny\Cache package
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Berny\Cache\Storage\ArrayAccess;
+
+use Berny\Cache\ItemInterface;
+
+class Item implements ItemInterface
+{
+    private $key;
+    private $array;
+
+    public function __construct($key, \ArrayAccess $array)
+    {
+        $this->key = $key;
+        $this->array = $array;
+    }
+
+    public function get()
+    {
+        return $this->array[$this->key];
+    }
+
+    public function key()
+    {
+        return $this->key;
+    }
+
+    public function miss()
+    {
+        return !isset($this->array[$this->key]);
+    }
+
+    public function remove()
+    {
+        unset($this->array[$this->key]);
+        return $this;
+    }
+
+    public function set($value, $ttl = null)
+    {
+        return $this->array[$this->key] = $value;
+    }
+}
---
+++ src/Berny/Cache/Storage/ArrayAccessStorage.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Berny\Cache package
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Berny\Cache\Storage;
+
+use ArrayAccess;
+use Berny\Cache\StorageInterface;
+
+class ArrayAccessStorage implements StorageInterface
+{
+    private $storage;
+
+    public function __construct(ArrayAccess $storage = null)
+    {
+        $this->storage = $storage ?: array();
+    }
+
+    public function getItem($key)
+    {
+        return new ArrayAccess\Item($key, $this->storage);
+    }
+
+    public function getStorage()
+    {
+        return $this->storage;
+    }
+}
